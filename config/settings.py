c408492067c04cbe5ab780911486d09057448d6d
ihor <lsujh@i.ua>
2021-01-23 10:49:12 +0200

    1

--- config/settings.py
+++ config/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config("SECRET_KEY", default="very-secret-key")
 DEBUG = config("DEBUG", default=True, cast=bool)
 
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', '*.herokuapp.com']
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'blog-agregator.herokuapp.com']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
