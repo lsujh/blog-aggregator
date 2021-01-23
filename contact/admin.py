a6b8d5bdf6534278b651e83a414e11cfa473634e
ihor <lsujh@i.ua>
2021-01-23 14:04:15 +0200

    1

--- contact/admin.py
+++ contact/admin.py
@@ -4,7 +4,7 @@ from .models import Contact
 
 @admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = ('email', 'title', 'message', 'files', 'created',)
+    list_display = ('email', 'title', 'files', 'created',)
     list_display_links = ('email',)
     ordering = ('-created',)
 
